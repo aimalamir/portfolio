74a0fba8c7eb2ada269c027e36901253ae2a3e6b
Aimal Amir <aimalamir@Aimals-Air.domain.name>
2024-07-26 00:03:03 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"], // Adjust this as needed for your project
+  theme: {
+    extend: {
+      colors: {
+        'custom-dark-blue': '#09090b',
+      },
+    },
+  },
+  plugins: [],
+}
